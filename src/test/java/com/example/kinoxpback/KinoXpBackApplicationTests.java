6614dd9eb360bfe7ed1a561084e0fd1ab4c245e0
Jesper Erling Nielsen <jesperenielsen99@gmail.com>
2023-09-26 15:34:55 +0200

    initial commit

---
+++ src/test/java/com/example/kinoxpback/KinoXpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinoxpback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
