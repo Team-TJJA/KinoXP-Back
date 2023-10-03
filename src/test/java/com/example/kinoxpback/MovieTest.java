dbd42a851efb8c471abd90469a00aad4ea479169
Jesper Erling Nielsen <jesp74m8@stud.kea.dk>
2023-10-03 09:40:33 +0200

    pushing just in case

--- src/test/java/com/example/kinoxpback/MovieTest.java
+++ src/test/java/com/example/kinoxpback/MovieTest.java
@@ -1,21 +1,11 @@
 package com.example.kinoxpback;
 
-//import com.example.kinoxpback.model.Movie;
 import com.example.kinoxpback.model.Movie;
-import com.example.kinoxpback.repository.MovieRepository;
 import com.example.kinoxpback.service.Impl.MovieServiceImpl;
-import com.example.kinoxpback.service.MovieService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
