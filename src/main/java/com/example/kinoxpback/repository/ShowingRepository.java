2b48bc7445874d8e66f070a3b6a6094b95201ddb
Hjordten <andr314e@stud.kea.dk>
2023-10-09 12:30:36 +0200

    added endpoint to get showings for a spicific date

--- src/main/java/com/example/kinoxpback/repository/ShowingRepository.java
+++ src/main/java/com/example/kinoxpback/repository/ShowingRepository.java
@@ -2,7 +2,14 @@ package com.example.kinoxpback.repository;
 
 import com.example.kinoxpback.model.Showing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
 
 public interface ShowingRepository extends JpaRepository<Showing,Integer> {
+    @Query("SELECT s FROM Showing s WHERE s.showingDate = :date")
+    List<Showing> getShowingsFromDate(@Param("date") Date date);
 
 }
